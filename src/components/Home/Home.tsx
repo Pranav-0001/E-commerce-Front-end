bcd30bb3d9d15cb6cf3bc35fe47bf52b802f8600
Pranav-0001 <pranavchakkalakkal@gmail.com>
2023-10-20 14:10:14 +0530

    In Search box enter key  to navigate

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
       </div>
       <div className='w-full flex justify-center px-6 pt-10 '>
         <div className='lg:w-1/2 md:w-3/4 w-full  bg-white h-10 rounded-lg px-4 flex items-center'>
-          <input type="text"  className='h-full bg-transparent outline-none w-full' onFocus={()=>setisSuggesionOpen(true)} onBlur={()=>setisSuggesionOpen(false)}/>
+          <input type="text"  className='h-full bg-transparent outline-none w-full' onKeyPress={(e)=>e.key=='Enter'?navigate('/products'):null} onFocus={()=>setisSuggesionOpen(true)} onBlur={()=>setisSuggesionOpen(false)}/>
           <FontAwesomeIcon onClick={()=>navigate('/products')} icon={faSearch} className='text-gray-500 text-xl'/>
         </div>
         
